6672c2e2651914f5495228f4d0f6d40e7e9a8946
devvarth Singh <devvarthsinghwork@gmail.com>
2026-09-11 15:30:40 +0530

    setup organisation feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import { UserButton } from "@clerk/nextjs"
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs"
 import { auth } from "@clerk/nextjs/server"
 import {
   Empty,
@@ -27,6 +27,7 @@ export default async function Page() {
         </EmptyHeader>
       </Empty>
       <UserButton />
+      <OrganizationSwitcher/>
     </div>
   )
 }
